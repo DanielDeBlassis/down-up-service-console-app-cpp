7b8bb23d58bbf21b3edc4972e70b3afce77340c2
Daniel <98892382+DanielDeBlassis@users.noreply.github.com>
2023-07-01 09:31:26 -0300

    Define colores

---
+++ colores.h
@@ -0,0 +1,19 @@
+#ifndef COLORES_H
+#define COLORES_H
+
+namespace Colores
+{
+// Códigos de escape ANSI para los colores
+#define RESET "\033[0m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define WHITE "\033[97m"
+#define LIGHT_YELLOW "\033[93m"
+#define LIGHT_CYAN "\033[96m"
+#define LIGHT_GREEN "\033[92m"
+#define LIGHT_MAGENTA "\033[95m"
+}
+
+#endif
